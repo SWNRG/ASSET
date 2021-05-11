a3eb9da1682945f52de1bf3142ad3a134ea5ab3b
georgevio <georgevio@gmail.com>
2021-05-11 19:21:16 +0300

    trying to fix Serial port probing. Unified the search in one method

--- src/com/uom/georgevio/SerialPortProbe.java
+++ src/com/uom/georgevio/SerialPortProbe.java
@@ -13,57 +13,52 @@ public class SerialPortProbe {
 	public SerialPort returnSerialPort() {
 		return motePort;
 	}
-	
-	//TODO: Rewrite with a while loop
-	public SerialPort getSerialPort() {
-		
-		
-		String iVal = "";
-		String portString = "dev/pts/";
-		
-		while (motePort == null) {
+
+	public SerialPort getSerialPort() {		
+		StringBuilder portString = new StringBuilder();
+		do {
 			for (int i=1; i<20; i++) {
-				 iVal=String.valueOf(i);
-				 portString+=iVal;
-				 System.out.println("Trying port: "+portString);
-				 motePort=findPort(portString);
-				 portString = "dev/pts/";
+				portString.append("dev/pts/");
+				portString.append(String.valueOf(i));
+				debug("Trying port: "+portString);				
+				try{
+					/********* Set & open the serial port ***************************/            
+					//debug("Opening port:"+ portName);
+					motePort = SerialPort.getCommPort(portString.toString());
+					motePort.closePort();
+					motePort.setBaudRate(115200);
+					//motePort.setParity(1);
+					motePort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+					if(motePort.openPort()){
+						debug("Serial Port found: "+motePort.getDescriptivePortName());
+						//debug("Baud Rate:"+ motePort.getBaudRate());
+						//debug(" Parity:"+ motePort.getParity());
+						//debug(" Write-Timeout:"+ motePort.getWriteTimeout());
+						return motePort;
+					} else {
+						//debug("Serial Port not found. Check if port number exists in SerialPortProbe.java");
+						//debug("Going to sleep for 300ms");
+						Thread.sleep(300);
+						//return null;
+					}		
+				} catch (Exception e) {
+					debug(e.toString());
+				} 
+				//motePort=findPort(portString.toString());
+				portString.setLength(0);
+				portString.append("dev/pts/");
 			}
-		}
+		}while (motePort == null); 
+		//debug("OPEN PORT FOUND...");
 	/********* Set & open the serial port ***************************/      
-		/* if (motePort == null) 
-			//motePort=findPort("dev/pts/1");
-		//if (motePort == null) 
-			//motePort=findPort("dev/pts/2");
-		if (motePort == null)
-			motePort=findPort("dev/pts/3");
-		if (motePort == null)
-			motePort=findPort("dev/pts/4");
-		if (motePort == null)
-			motePort=findPort("dev/pts/6");
-		if (motePort == null)
-			motePort=findPort("dev/pts/7");
-		if (motePort == null)	
-			motePort=findPort("dev/pts/14");
-		//if (motePort == null)	
-			//motePort=findPort("dev/pts/15");
-		if (motePort == null)	
-			motePort=findPort("dev/pts/17");
-		if (motePort == null)
-			motePort=findPort("dev/pts/18");
-		if (motePort == null)
-			motePort = findPort("dev/pts/19");
-		if (motePort == null)
-			motePort=findPort("dev/pts/20");
-		if (motePort == null)
-			motePort=findPort("dev/pts/21"); */
-		return motePort;
+		//return motePort;
 	}
+	
 /***********METHODS*******************************************/	
 	protected SerialPort findPort(String portName ) {
 		try{
 			/********* Set & open the serial port ***************************/            
-			debug("Opening port:"+ portName);
+			//debug("Opening port:"+ portName);
 			motePort = SerialPort.getCommPort(portName);
 			motePort.closePort();
 			motePort.setBaudRate(115200);
@@ -87,6 +82,7 @@ public class SerialPortProbe {
 		return null;
 	}
 	
+/***********UNIVRSAL PRINT IN GUI OUTPUT ***********************************/		
     private static void debug(String message){
     	Main.debug((message));
 	}    				
